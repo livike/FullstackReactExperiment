ab7e5071b323105944e22de884792352337baa8d
Livia Dobai <livia.dobai@moon42.com>
2021-06-03 14:39:14 +0200

    MicroORM and Typeqraphql crud

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,17 +1,23 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, ObjectType, Int } from "type-graphql";
 
+// make it a graphql type
+@ObjectType()
 @Entity()
 export class Post {
-
+    @Field(() => Int)
     @PrimaryKey()
     id!: number;
-  
+    
+    @Field(() => String)
     @Property()
     createdAt: Date = new Date();
   
+    @Field(() => String)
     @Property({ onUpdate: () => new Date() })
     updatedAt: Date = new Date();
   
+    @Field()
     @Property({type: 'text'})
     title!: string;
 
